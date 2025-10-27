59b22e98f22deb44476177a2ed26cd1a66996988
hetvit <hetvit@g.ucla.edu>
2025-10-27 16:03:22 -0700

    Initial Commit

---
+++ Capstone2026_2/Capstone2026_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Capstone2026_2App.swift
+//  Capstone2026_2
+//
+//  Created by Hetvi Trivedi on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Capstone2026_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
